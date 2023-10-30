ccf9b38f29e6ebdfc1645050f3c2d52c16a90feb
Nayandhika Rebeca Paul <sunilpaul@Nayandhikas-MacBook-Pro.local>
2023-10-30 23:25:45 +0530

    Added

--- scrape.py
+++ scrape.py
@@ -1,7 +1,26 @@
+# from bs4 import BeautifulSoup
+# from selenium import webdriver
+
+# url = "https://ellicium.com/blog/5-efficient-ways-to-extract-text-from-articles/#:~:text=Html2text,designed%20for%20article%20text%20extraction."
+
+# driver = webdriver.Chrome()  
+
+# driver.get(url)
+
+# html = driver.page_source
+
+# soup = BeautifulSoup(html, 'html.parser')
+
+# paragraphs = soup.find_all(['li','p','h1','h2','h3','h4','h5','h6'])
+# for element in paragraphs:
+#     print(element.get_text())
+
+# driver.quit()
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-url = "https://ellicium.com/blog/5-efficient-ways-to-extract-text-from-articles/#:~:text=Html2text,designed%20for%20article%20text%20extraction."
+url = "https://www.brookings.edu/articles/how-artificial-intelligence-is-transforming-the-world/"  
 
 driver = webdriver.Chrome()  
 
@@ -11,8 +30,9 @@ html = driver.page_source
 
 soup = BeautifulSoup(html, 'html.parser')
 
-paragraphs = soup.find_all(['li','p','h1','h2','h3','h4','h5','h6'])
-for element in paragraphs:
-    print(element.get_text())
+text = soup.get_text()
+
+print(text)
 
 driver.quit()
+
