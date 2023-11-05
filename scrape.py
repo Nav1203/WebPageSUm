be3aa26563df2c357a175db42c0692c6552b20ec
Nayandhika Rebeca Paul <sunilpaul@Nayandhikas-MacBook-Pro.local>
2023-11-05 12:27:23 +0530

    modified scrape.py

--- scrape.py
+++ scrape.py
@@ -29,20 +29,58 @@ class GetWebpageData:
         text = soup.get_text()
         return text
 
-
+# import requests
+# from bs4 import BeautifulSoup
 # url = "https://www.isro.gov.in/Making_Chandrayaan3_ISRO_culture.html"  
+# response = requests.get(url)
+# if response.status_code == 200:
+#     soup = BeautifulSoup(response.content, 'html.parser')
+#     all_elements = []
+#     lines = soup.find_all('li',class_ = False,id = False)
+#     paragraphs = soup.find_all('p',id = False)
+#     title = soup.find('title')
+#     headings = soup.find_all(['h1', 'h2', 'h3', 'h4'])
+#     for line in lines:
+#         if not line.find_all():
+#             all_elements.append(line)
 
-# driver = webdriver.Chrome()  
+#     all_elements.extend(paragraphs)
+#     if title:
+#         all_elements.append(title)
+#     all_elements.extend(headings)
 
-# driver.get(url)
+#     for element in all_elements:
+#         text = element.get_text(strip=True)
+#         print(text)
 
-# html = driver.page_source
+# else:
+#     print(f"Failed to retrieve the web page. Status code: {response.status_code}")
 
-# soup = BeautifulSoup(html, 'html.parser')
-
-# text = soup.get_text()
-
-# print(text)
-
-# driver.quit()
-
+import requests
+from bs4 import BeautifulSoup
+url = "https://www.brookings.edu/articles/how-artificial-intelligence-is-transforming-the-world/"
+response = requests.get(url)
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, 'html.parser')
+    elements = soup.descendants
+    all_elements = []
+    def has_span_ancestor(element):
+        return any(ancestor.name == 'span' for ancestor in element.parents)
+    for element in elements:
+        if element.name == 'li' and not element.find_all() and not element.has_attr('class') and not element.has_attr('id') and not has_span_ancestor(element):
+            text = element.get_text(strip=True)
+            all_elements.append(text)
+        if element.name in ['title','h1','h2','h3','h4'] and not has_span_ancestor(element):
+            text = element.get_text(strip=True)
+            all_elements.append(text)
+        if element.name == 'p' and not element.has_attr('id') and not has_span_ancestor(element):
+            text = element.get_text(strip=True)
+            all_elements.append(text)
+    for element in all_elements:
+        print(element)
+    # elements = soup.find_all(['p','li','h1','h2','h3','h4'])
+    # for element in elements:
+    #     text = element.get_text(strip=True)
+    #     print(text)
+else:
+    print(f"Failed to retrieve the web page. Status code: {response.status_code}")
